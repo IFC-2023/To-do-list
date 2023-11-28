c60e56e6ffa5daf98370e48cde3b6247761e269f
LuizBlume <luiz.fernando.251107@gmail.com>
2023-11-28 16:35:14 -0300

    mds, vou chorar

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let tarefas = document.querySelector("#inputTarefa");
-let itens = document.querySelector("#addItens");
-let botao = document.querySelector("#botao");
-
 function addItens() {
-    resultado.innerHTML = tarefas.value;
-    selecionados = document.createElement("li");
-    selecionados.innerHTML = resultado; 
+    let resultado = document.getElementById("resultado").value;
+    let selecionados = document.createElement("li");
+    selecionados.innerHTML = resultado;
+    document.getElementById("item").appendChild(selecionados);
+    if (selecionados > 0) {
+        let finalizar = document.createElement("a");
+        finalizar.innerHTML = resultado;
+        document.getElementById("finalizar").appendChild(finalizar);
+    }
 }    
 // resultado.innerHTML = tarefas.value;
+
+document.getElementById("button").addEventListener("click", addItens);
