55f91e668cd8af39528345316ce44a4fd409eb76
LuizBlume <luiz.fernando.251107@gmail.com>
2023-11-29 16:57:58 -0300

    hotfix: sla o que o sor fez

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+let resultado = document.getElementById("resultado").value;
+let pendentes = document.getElementById("item");
+let finalizadas = document.getElementById("finalizadas");
+
 function addItens() {
-    let resultado = document.getElementById("resultado").value;
     let selecionados = document.createElement("li");
+    let ultimoItem = pendentes.childElementCount;
+    selecionados.dataset.index = ultimoItem;
     selecionados.innerHTML = resultado;
     document.getElementById("item").appendChild(selecionados);
     if (selecionados > 0) {
         let finalizar = document.createElement("a");
         finalizar.innerHTML = resultado;
         document.getElementById("finalizar").appendChild(finalizar);
+        document.getElementById("button").addEventListener
     }
 }    
 // resultado.innerHTML = tarefas.value;
 
 document.getElementById("button").addEventListener("click", addItens);
+
+function concluir(item) {
+    removido = pendentes.children[item];
+    pendentes.removeChild(removido);
+}
